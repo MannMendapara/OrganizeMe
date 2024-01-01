4d5e42a327c2b419ce281e3da3b001c2260333ee
Lalakiya Priyanka <priyankalalkiya192@gmail.com>
2024-01-01 22:31:23 +0530

    sidebar added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Sidebar from './Components/Sidebar/Sidebar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar/>
+    </>
+  )
+}
+export default App
