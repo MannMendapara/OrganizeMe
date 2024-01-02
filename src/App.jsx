9039662131a5f138c49fcb3445b9de0ae603611d
MannMendapara <109096692+MannMendapara@users.noreply.github.com>
2024-01-02 21:38:16 +0530

    Add functionality of sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
-import Sidebar from './Components/Sidebar/Sidebar'
+import Task_Card from './Components/Task_Card/Task_Card'
 
 function App() {
   return (
     <>
       <Navbar />
-      <Sidebar/>
+      {/* <Task_Card /> */}
     </>
   )
 }
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,23 +1,22 @@
-import React from "react";
 import './Sidebar.css'
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="dashboard">
-        <img src="./public/Images/dashboard.png"   alt="Icon" />
+        <img src="./Images/dashboard.png"   alt="Icon" />
         <p>Dashboard</p>
       </div>
       <div className="all-task">
-        <img src="./public/Images/alltask.png" alt="Icon" />
+        <img src="./Images/alltask.png" alt="Icon" />
         <p>All Task</p>
       </div>
       <div className="cmplt-task">
-        <img src="./public/Images/cmp-task.png" alt="Icon" />
+        <img src="./Images/cmp-task.png" alt="Icon" />
         <p>Complited Task</p>
       </div>
       <div className="add-task">
-        <img src="./public/Images/addtask.png" alt="Icon" />
+        <img src="./Images/addtask.png" alt="Icon" />
         <p>Add Task</p>
       </div>
     </div>
