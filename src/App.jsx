e95d0a2bae2f4497dcc9c694d698a9b2d6bec584
MannMendapara <109096692+MannMendapara@users.noreply.github.com>
2024-01-02 23:41:19 +0530

    Start Edit-Task page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Task_Card from './Components/Task_Card/Task_Card'
+import Edit_Task from './Components/Edit_Task/Edit_Task'
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <Task_Card /> */}
+      {/* <Testing /> */}
+      <Edit_Task />
     </>
   )
 }
---
+++ src/Components/Testing/Testing.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Testing.css'
+
+const Testing = () => {
+    return (
+        <div className='model'>
+            <div className="cont">
+                <div className="input-field">
+                    <label htmlFor="Task-Title">Task-Title</label>
+                    <br />
+                    <input type="text" id='Task-Title' placeholder='Task-Title' />
+                </div>
+                <div className="input-field">
+                    <label htmlFor="Start-date">Start-Date</label>
+                    <br />
+                    <input type="date" id='Start-date' placeholder='Start-Date' />
+                </div>
+                <div className="input-field">
+                    <label htmlFor="End-date">End-Date</label>
+                    <br />
+                    <input type="date" id='End-date' placeholder='End-Date' />
+                </div>
+                <div className="input-field">
+                    <label htmlFor="Priority">Priority</label>
+                    <br />
+                    <select type="text" id='Priority'>
+                        <option value="" disabled selected>Choose Priority</option>
+                        <option value="">Very Important</option>
+                        <option value="">Important</option>
+                        <option value="">Less Important</option>
+                    </select>
+                </div>
+                <div className="input-field">
+                    <label htmlFor="Category">Task-Title</label>
+                    <br />
+                    <input type="text" id='Category' placeholder='Category'/>
+                </div>
+                <div className='Text-Area'>
+                    <label htmlFor="Task-desc">Task-Description</label>
+                    <br />
+                    <textarea name="" id="Task-desc" cols="30" rows="10" placeholder='Write here' className='Task-desc'></textarea>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Testing
