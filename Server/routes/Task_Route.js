224f4ae7f69f1830834dd89e422820ad739821dc
MannMendapara <109096692+MannMendapara@users.noreply.github.com>
2024-01-16 17:44:11 +0530

    Implement Edit task and delete task

--- Server/routes/Task_Route.js
+++ Server/routes/Task_Route.js
@@ -1,8 +1,10 @@
 import express, { response } from "express";
 import Task from "../models/Task_Model.js";
+import mongoose from "mongoose";
 
 const Task_router = express.Router();
 
+//Fetched all the tasks from database
 Task_router.get("/", async (req, res) => {
   try {
     const data = await Task.find({});
@@ -13,6 +15,7 @@ Task_router.get("/", async (req, res) => {
   }
 });
 
+// Add the task to database
 Task_router.post("/add", async (req, res) => {
   try {
     const { Title, StartDate, EndDate, Status } = req.body;
@@ -32,6 +35,62 @@ Task_router.post("/add", async (req, res) => {
     res.status(201).json(newTask);
   } catch (error) {
     console.error("Error adding task:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Update the task in database
+Task_router.put("/update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { Title, StartDate, EndDate, Status } = req.body;
+
+    // Check For Empty field
+    if (!Title || !StartDate || !EndDate || !Status) {
+      return res
+        .status(400)
+        .json({ error: "Please provide all required fields" });
+    }
+
+    // Update Task by ID
+    const updatedTask = await Task.findByIdAndUpdate(
+      id,
+      { Title, StartDate, EndDate, Status },
+      { new: true } // Return the updated document
+    );
+
+    if (!updatedTask) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    console.error("Error updating task:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// delete the task
+Task_router.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Check if the provided ID is valid
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "Invalid ID format" });
+    }
+
+    // Find and delete the task by ID
+    const deletedTask = await Task.findByIdAndDelete(id);
+
+    // Check if the task was found and deleted
+    if (!deletedTask) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    res.status(200).json({ message: "Task deleted successfully", deletedTask });
+  } catch (error) {
+    console.error("Error deleting task:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
