b304c2fc1aca449840bd1afbcef78413c28f6e67
MannMendapara <109096692+MannMendapara@users.noreply.github.com>
2024-02-01 23:40:05 +0530

    Remove config/default.json

--- Server/routes/Auth.js
+++ Server/routes/Auth.js
@@ -60,7 +60,7 @@ Auth_Router.post(
 
       jwt.sign(
         payload,
-        config.get("jwtSecret"),
+        process.env.jwtSecret,
         { expiresIn: 360000 }, // Change to 3600 during production
         (err, token) => {
           if (err) throw err;
--- Server/routes/user.js
+++ Server/routes/user.js
@@ -46,7 +46,7 @@ User_Router.post(
       };
       jwt.sign(
         payload,
-        config.get("jwtSecret"),
+        process.env.jwtSecret,
         { expiresIn: 360000 }, // Change to 3600 during production
         (err, token) => {
           if (err) throw err;
